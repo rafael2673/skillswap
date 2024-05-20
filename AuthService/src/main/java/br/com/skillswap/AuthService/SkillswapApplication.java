240c623385971ca050be4e336775684cf5d606e4
Rafael Franco <rafaelribeirofranco4@gmail.com>
2024-05-20 17:18:13 -0300

    Microserviço de autenticação

---
+++ AuthService/src/main/java/br/com/skillswap/AuthService/SkillswapApplication.java
@@ -0,0 +1,13 @@
+package br.com.skillswap.AuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillswapApplication.class, args);
+	}
+
+}
