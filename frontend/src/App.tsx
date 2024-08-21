0bce625aa1f0496658b1aa2a1ea537f99eed4161
Rafael Franco <rafaelribeirofranco4@gmail.com>
2024-08-20 22:44:06 -0300

    Definindo as estruturas da página inicial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ConfigProvider, Layout, Button, Row, Col } from "antd";
+import logo from "./images/SkillswapLogo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Content } = Layout;
+
+const App: React.FC = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: "#6F42C1",
+        borderRadius: 6,
+
+        // Alias Token
+        colorBgContainer: "#FFFFFF",
+      },
+    }}
+  >
+    <Header className="header">
+      <div className="demo-logo">
+        <img src={logo} alt="Logo Skillswap" width={64} height={64} />
+      </div>
+      <Button type="primary">Junte-se ao Skillswap</Button>
+    </Header>
+    <Content>
+      <Row>
+        <Col className="session1" flex="auto">
+          <div>
+            <h1>Skillswap: </h1>
+            <h1>Compartilhe suas habilidades, aprenda algo novo.</h1>
+            <p>
+              Skillswap é uma plataforma inovadora que conecta pessoas dentro da
+              sua comunidade para que elas compartilhem seus conhecimentos e
+              aprendam juntas, sem a necessidade de dinheiro. Imagine um mundo
+              onde cada um pode ensinar o que sabe e aprender algo novo em
+              troca, apenas por amor ao aprendizado!
+            </p>
+            <Button type="primary">Junte-se ao Skillswap</Button>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col flex="auto" className="session2">
+          <div>
+            <h2>Despertando o potencial da comunidade</h2>
+          </div>
+        </Col>
+      </Row>
+    </Content>
+  </ConfigProvider>
+);
 
 export default App;
